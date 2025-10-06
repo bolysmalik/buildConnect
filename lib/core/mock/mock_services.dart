6bc71679b3c11c2b5b22608ae0b2b71fd9c11dc6
oral-ushk <220107058@stu.sdu.edu.kz>
2025-10-06 15:40:00 +0500

    Connection of mocks

---
+++ lib/core/mock/mock_services.dart
@@ -0,0 +1,8 @@
+// mock_services.dart
+// Экспорт всех mock сервисов для удобного импорта
+
+export 'mock_database.dart';
+export 'mock_auth_service.dart';
+export 'mock_requests_service.dart';
+export 'mock_chat_service.dart';
+export 'mock_service_manager.dart';
